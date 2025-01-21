91db618ab0beebf541d60c720d87d3a1086f5b11
Ignacio Fleming <ignacioflemings@gmail.com>
2025-01-21 19:49:00 -0300

    ..

--- src/pages/Home/HomeBackground/HomeBackground.jsx
+++ src/pages/Home/HomeBackground/HomeBackground.jsx
@@ -3,7 +3,7 @@ import styles from "../Home.module.css";
 function HomeBackground() {
   return (
     <video autoPlay muted loop playsInline className={styles.backgroundVideo}>
-      <source src="/public/videos/blueNeonBG.mp4" type="video/mp4" />
+      <source src="/videos/blueNeonBG.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
