f66efd884bf06ac4ca84d0a9aee0ed191718e47b
Ignacio Fleming <ignacioflemings@gmail.com>
2025-02-08 19:53:00 -0300

    add bg video animation

--- src/pages/Home/HomeBackground/HomeBackground.jsx
+++ src/pages/Home/HomeBackground/HomeBackground.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import styles from "../Home.module.css";
 
 function HomeBackground() {
+  const [videoLoaded, setVideoLoaded] = useState(false);
+  const handleVideoLoaded = () => {
+    setVideoLoaded(true);
+  };
   return (
-    <video autoPlay muted loop playsInline className={styles.backgroundVideo}>
-      <source src="/videos/blueNeonBG.webm" type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
+    <div className={styles.backgroundWrapper}>
+      <video preload="auto" autoPlay muted loop playsInline onLoadedData={handleVideoLoaded} className={styles.backgroundVideo}>
+        <source src="/videos/blueNeonBG.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   );
 }
 
