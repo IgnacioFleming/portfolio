87f72479390f5be2fb6cb710a73e31a1eb6a801a
Ignacio Fleming <ignacioflemings@gmail.com>
2025-02-10 19:53:00 -0300

    add source code to projects

--- src/pages/Projects/renderFooter.jsx
+++ src/pages/Projects/renderFooter.jsx
@@ -1,11 +1,11 @@
-import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaExternalLinkAlt, FaCode } from "react-icons/fa";
 import Button from "../../components/Button/Button";
 import { icons } from "../../helpers/icons";
 
 export const renderFooter = (project) => {
   return (
-    <footer className="flex flex-col gap-2 justify-end">
-      <div className="flex flex-col items-start gap-3">
+    <footer className="flex flex-col gap-1 justify-end">
+      <div className="flex flex-col items-start gap-2">
         <div className="flex gap-2">
           {project.stack.map((tech, index) => {
             return (
@@ -15,9 +15,23 @@ export const renderFooter = (project) => {
             );
           })}
         </div>
-        <a href={project.deploy} target="_blank">
-          <Button label="WEBSITE" className="text-primary hover:text-black font-semibold border-none h-10 w-auto shadow-overWhite bg-dark" icon={<FaExternalLinkAlt />} iconPosition="right" />
-        </a>
+        <div className="flex justify-between w-full items-center h-12">
+          <a href={project.deploy} target="_blank" className="flex items-center">
+            <Button label="DEPLOY" className="text-primary hover:text-black font-semibold border-none h-10 w-auto shadow-overWhite bg-dark" icon={<FaExternalLinkAlt />} iconPosition="right" />
+          </a>
+          <div className="flex flex-col gap-1">
+            <a href={project.frontEndRepositoryPath} target="_blank" className="flex gap-1 align-start text-sm">
+              <FaCode size={18} />
+              FrontEnd
+            </a>
+            {project.backEndRepositoryPath && (
+              <a href={project.backEndRepositoryPath} target="_blank" className="flex gap-1 align-start text-sm">
+                <FaCode size={18} />
+                BackEnd
+              </a>
+            )}
+          </div>
+        </div>
       </div>
     </footer>
   );
