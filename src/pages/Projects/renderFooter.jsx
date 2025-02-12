845633af3e24f0fecdefce7981727be1b2902866
Ignacio Fleming <ignacioflemings@gmail.com>
2025-02-12 19:53:00 -0300

    comment code from github

--- src/pages/Projects/renderFooter.jsx
+++ src/pages/Projects/renderFooter.jsx
@@ -19,6 +19,8 @@ export const renderFooter = (project) => {
           <a href={project.deploy} target="_blank" className="flex items-center">
             <Button label="WEBSITE" className="text-primary hover:text-black font-semibold border-none h-10 w-auto shadow-overWhite bg-dark" icon={<FaExternalLinkAlt />} iconPosition="right" />
           </a>
+          {/* 
+          comment this code temporaly
           <div className="flex flex-col gap-1 h-10">
             <a href={project.frontEndRepositoryPath} target="_blank" className="flex gap-1 align-start text-sm">
               <FaCode size={18} />
@@ -30,7 +32,7 @@ export const renderFooter = (project) => {
                 BackEnd
               </a>
             )}
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
