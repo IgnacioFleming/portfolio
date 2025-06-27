e7fafd5466bdf73dc55deef5fafde004a02001ac
Ignacio Fleming <ignacioflemings@gmail.com>
2025-06-27 11:30:35 -0300

    a

--- src/pages/Contact/ContactContainer.jsx
+++ src/pages/Contact/ContactContainer.jsx
@@ -23,36 +23,36 @@ const ContactoContainer = ({ fontStyles }) => {
     let query = { ...data, fecha: Date() };
 
     const queriesCollection = collection(db, "queries");
-    await addDoc(queriesCollection, query);
-    // .then(() => {
-    //   fetch(`${import.meta.env.VITE_APP_URL_BASE}/api/sendEmail`, {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       transporter: "vizcacha",
-    //       subject: `Nueva consulta de ${query.nombre}`,
-    //       html: emailTemplate(query.nombre, query.email, query.consulta),
-    //     }),
-    //   });
-    Swal.fire({
-      title: "Message sent!",
-      text: "Your message has been sent, you will soon receive a reply in your email. Thank you!",
-      position: "center",
-      icon: "success",
-      confirmButtonColor: "#1e4477",
-    });
-    // })
-    // .catch(() => {
-    //   Swal.fire({
-    //     title: "Sorry..",
-    //     text: "An error ocurred while trying to send your message, please write an email to ignacioflemings@gmail.com. Thank you!",
-    //     position: "center",
-    //     icon: "error",
-    //     confirmButtonColor: "#1e4477",
-    //   });
-    // });
+    try {
+      await addDoc(queriesCollection, query);
+
+      await fetch(`${import.meta.env.VITE_APP_URL_BASE}/api/sendEmail`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          transporter: "vizcacha",
+          subject: `Nueva consulta de ${query.nombre}`,
+          html: emailTemplate(query.nombre, query.email, query.consulta),
+        }),
+      });
+      Swal.fire({
+        title: "Message sent!",
+        text: "Your message has been sent, you will soon receive a reply in your email. Thank you!",
+        position: "center",
+        icon: "success",
+        confirmButtonColor: "#1e4477",
+      });
+    } catch (error) {
+      Swal.fire({
+        title: "Sorry..",
+        text: "An error ocurred while trying to send your message, please write an email to ignacioflemings@gmail.com. Thank you!",
+        position: "center",
+        icon: "error",
+        confirmButtonColor: "#1e4477",
+      });
+    }
     resetForm();
   }
 
